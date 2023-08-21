0515ae9dd9c3aeb046734029a8e1523f5367f7d5
AkshathaXwordz <akshathaxwordz@gmail.com>
2023-08-21 11:38:17 +0530

    committing  array examples SuperMarket and Bank and Bag

---
+++ Bag.java
@@ -0,0 +1,5 @@
+class Bag{
+public static void main (String akshu[]){
+System.out.println(" don't use plastic bags.");
+}
+}
---
+++ Bank.java
@@ -0,0 +1,5 @@
+class Bank {
+public static void main (String akshu[]){
+System.out.println("an organization where people and businesses can invest or borrow money.");
+}
+}
