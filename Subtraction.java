580776b0e47efd5e0def7ded8fe95ca3adc25670
AkshathaXwordz <akshathaxwordz@gmail.com>
2023-08-22 11:46:51 +0530

    examples of arrays

---
+++ Subtraction.java
@@ -0,0 +1,29 @@
+class Subtraction {
+	 public static void main(String subtraction[])
+	  {
+	       subtract(70,20);
+		   subtract(70,30);
+		   subtract(60,30);
+		   subtract(30,20);
+		   subtract(70,10);
+		   subtract(90,20);
+		   subtract(100,10);
+		   subtract(50,20);
+		   subtract(70,60);
+		   subtract(54,22);
+		    subtract  (80,20);
+		   subtract(85,25);
+		   subtract(72,23);
+		   subtract(83,33);
+		   subtract(99,20);
+		   subtract(70,50);
+		   subtract(76,26);
+		   subtract(49,89);
+		   subtract(22,33);
+		   subtract(78,28);
+		   
+		   }
+		    
+			public static void subtract(int a, int b){
+			 System .out.println(a-b);}
+			 }
---
+++ Voot.java
@@ -0,0 +1,43 @@
+class Voot {
+static String englishMovies[]  = {"Avengers-End", "Titanic","Sceam6","OPenHimer","Spiderman","The King"};
+static String kannadaMovies[]  = {"kranti", "Kurushetra","kantara","KGf","Yajamana","Kariya"};
+static String hindiMovies[]  = {"Bawaal", "pathaan","Dhokha","Goodbye","Blurr","Doctor G"};
+static String telguMovies[]  = {"Agent", "Ravanasura","Dasara","Shaakuntalam","Dhamaka","spy"};
+	
+	public static void main(String movies[]){
+		{System.out.println("Main started");
+	
+	System.out.println("the collection of englishMovies are:");
+	System.out.println(englishMovies.length);	
+for ( int dev=0; dev< englishMovies.length; dev++ ){
+		System.out.println(englishMovies[dev]);
+}
+		System.out.println("Main ended");}
+
+
+		{System.out.println("Main started");
+	System.out.println("the collection of kannadaMovies are:");
+
+System.out.println(kannadaMovies.length);
+for ( int dev=0; dev< kannadaMovies.length; dev++ ){
+		System.out.println(kannadaMovies[dev]);
+		}System.out.println("Main ended");}
+
+		{
+System.out.println("Main started");
+System.out.println("the collection of hindiMovies are:");
+System.out.println(hindiMovies.length);
+for ( int dev=0; dev< hindiMovies.length; dev++ ){
+		System.out.println(hindiMovies[dev]);
+		}System.out.println("Main ended");}
+
+		{System.out.println("Main started");
+System.out.println("the collection of telguMovies are:");
+System.out.println(telguMovies.length);
+for ( int dev=0; dev< telguMovies.length; dev++ ){
+		System.out.println(telguMovies[dev]);
+}
+		System.out.println("Main ended");}
+	}
+}
+	 
