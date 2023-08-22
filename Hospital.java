1399175ff685a0b4bc94981f5345ab968b91be67
Akshatha v Hanasi <142565920+AkshathaXwordz@users.noreply.github.com>
2023-08-22 12:37:19 +0530

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,17 @@
+class Hospital{
+public static void main(String hospital[]){
+     String hospitalName="City Centre Hospital";
+	String sqrtFeet="12000sq.ft.";
+	String location ="Davangere";
+	int noOfFloor= 4;
+	String noOfBeds="more than 100";
+	boolean isParkingAvailable =true;
+	System.out.println("the Name of the Hospital-" + hospitalName);
+	System.out.println("the total retail floor area of hospital -"+sqrtfeet);
+	System.out.println("the hospital is located in-" + location);
+	System.out.println("Number of floor in the hospital-" + noOfFloor);
+	System.out.println("Number of beds in the hospital-" +noOfBeds);
+	System.out.println("Is Parking Available -" + isParkingAvailable);
+	}
+}
+	
