7f1e6d11e22df89823f7d6696bcf1c805853609f
Akshatha v Hanasi <142565920+AkshathaXwordz@users.noreply.github.com>
2023-08-22 12:38:05 +0530

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,17 @@
+class Hotel {
+	public static void main(String hotel[]){
+	
+	String hotelName="Sri kotureshwara Dose hotel";
+	String ownerName="Kaushik kumar";
+	String address="vidyanagar Road Davangere";
+	int noOfTables=26;
+	String foodMenu="Benne dosa,masala dosa,set dosa,plain dosa";
+	String ratings="5stars";
+	System.out.println("the name of the hotel is -" + hotelName);
+	System.out.println("the Owner of the hotel is -" + ownerName);
+	System.out.println("the hotel is located in -" + address);
+	System.out.println("the Number of tables in hotel are -" + noOfTables);
+    System.out.println("the Menu of the hotel - " +foodMenu );
+	System.out.println("Ratings of this hotel -" + ratings);
+	}
+}
