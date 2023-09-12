23281b06b0b109093a644e744beb322bc7a8d90f
AkshathaXwordz <akshathaxwordz@gmail.com>
2023-09-12 19:35:01 +0530

    examples of inheritance

--- BankEx.java
+++ BankEx.java
@@ -1,31 +1,7 @@
-class BankEx
-{
+class BankEx {
+    public static void main(String ban[]){
+	
+		SbiBank sbi =new  SbiBank();//object created
+		sbi.getAccount(1823,"gsha3843");//calling method with passing argument
 
- public  static void main ( String details[]){
- 
- Bank bank = new Bank();
- 
- bank.setBankName("Canara Bank");
- System.out.println("the bank name is " + bank.getBankName());
- 
- bank.setBranchName("VidyaNagar");
- System.out.println("the bank name is " + bank.getBranchName());
- 
- bank.setManegerName("Kaushik");
- System.out.println("the maneger name is " + bank.getManegerName());
- 
- bank.setAddress("Taralubalu Badavane Vidyanagar Davangere");
- System.out.println("the address " + bank.getAddress());
- 
- bank.setIFSCCode("CNRB0001813");
- System.out.println("the IFSC code of the bank  is " + bank.getIFSCCode());
- 
- bank.setMICRCode(577015007L);
- System.out.println("the MICR Code of the bank is " + bank.getMICRCode());
- 
- bank.setNoOfWorkers(7);
- System.out.println("the number of workers in bank  " + bank.getNoOfWorkers());
- 
- }
 }
- 
---
+++ OttplatformExecutor.java
@@ -0,0 +1,10 @@
+class OttplatformExecutor {
+ public static void main (String netflix[])
+ {
+String movies[]  = {"kranti", "Kurushetra","kantara","KGf","Yajamana","Kariya"};
+
+  Netflix  movie = new Netflix() ;
+ movie.getMovies( movies);
+ System.out.println(movie);
+ }
+ }
